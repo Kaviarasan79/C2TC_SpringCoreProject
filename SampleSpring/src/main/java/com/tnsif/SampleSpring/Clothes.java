afbd7b9b4e9550fffb264984e5cd30b08b8346f4
Kaviarasan79 <kaviarasan0709@gmail.com>
2025-09-10 15:31:12 +0530

    SampleSpring

---
+++ SampleSpring/src/main/java/com/tnsif/SampleSpring/Clothes.java
@@ -0,0 +1,11 @@
+package com.tnsif.SampleSpring;
+
+public class Clothes implements Shopping {
+
+	@Override
+	public void order() {
+		// TODO Auto-generated method stub
+		System.out.println("I'm ordering some dress");
+	}
+
+}
